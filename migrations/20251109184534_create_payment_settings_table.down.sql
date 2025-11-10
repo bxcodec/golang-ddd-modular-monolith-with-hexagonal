7b2ee66a2daaa6d438345a86486ddd8811a3f993
Iman Tumorang <iman@tumorang.com>
2025-11-10 18:48:17 +0200

    chore: remove duplicate

--- migrations/20251109184534_create_payment_settings_table.down.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS idx_payment_settings_created_at;
-DROP INDEX IF EXISTS idx_payment_settings_status;
-DROP INDEX IF EXISTS idx_payment_settings_currency;
-DROP TABLE IF EXISTS payment_settings;
-
